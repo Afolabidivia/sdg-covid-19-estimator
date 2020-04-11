87ba8f31b12aeb64ce6dd7e63598dbcfef8a0112
devDavid <princediviva4xuccess@gmail.com>
2020-04-11 14:52:10 +0100

    Challenge 1 and 2 removed from the code.

--- src/estimator.js
+++ src/estimator.js
@@ -1,64 +1,9 @@
-/* The second argument (reportedType) of this function is to determine
-    whether it is impact or severe impact
-    reportedType can only be 0 or 1 which is impact and severe impact respectively
-* */
-const currentlyInfectedPeople = (reportedCases, reportedType) => {
-  let infectedPeople;
-  if (reportedType === 0) {
-    infectedPeople = reportedCases * 10;
-  } else if (reportedType === 1) {
-    infectedPeople = reportedCases * 50;
-  }
-  return infectedPeople;
-};
-
-const sortDaysCalculation = (periodType, timeToElapse) => {
-  switch (periodType) {
-    case 'days':
-      return 2 * Math.trunc((timeToElapse / 3));
-    case 'weeks':
-      return 2 * Math.trunc((timeToElapse * 7) / 3);
-    case 'months':
-      return 2 * Math.trunc((timeToElapse * 30) / 3);
-    default:
-      return null;
-  }
-};
-
 const covid19ImpactEstimator = (data) => {
   const inputData = data;
-  // Impact variables declaration
-  const impactCurrentlyInfected = currentlyInfectedPeople(inputData.reportedCases, 0);
-  const impactInfectionsByRequestedTime = impactCurrentlyInfected
-    * sortDaysCalculation(inputData.periodType, inputData.timeToElapse);
-  const impactSevereCasesByRequestedTime = (15 / 100) * impactInfectionsByRequestedTime;
-  const impactHospitalBedsByRequestedTime = Math.trunc(
-    ((35 / 100) * inputData.totalHospitalBeds) - impactSevereCasesByRequestedTime
-  );
-
-  // Severe Impact variable declarations
-  const severeImpactCurrentlyInfected = currentlyInfectedPeople(data.reportedCases, 1);
-  const severeImpactInfectionsByRequestedTime = severeImpactCurrentlyInfected
-    * sortDaysCalculation(inputData.periodType, inputData.timeToElapse);
-  const severeImpactSevereCasesByRequestedTime = (15 / 100) * severeImpactInfectionsByRequestedTime;
-  const severeImpactHospitalBedsByRequestedTime = Math.trunc(
-    ((35 / 100) * inputData.totalHospitalBeds) - severeImpactSevereCasesByRequestedTime
-  );
-
   return {
     data: inputData,
-    impact: {
-      currentlyInfected: impactCurrentlyInfected,
-      infectionsByRequestedTime: impactInfectionsByRequestedTime,
-      severeCasesByRequestedTime: impactSevereCasesByRequestedTime,
-      hospitalBedsByRequestedTime: impactHospitalBedsByRequestedTime
-    },
-    severeImpact: {
-      currentlyInfected: severeImpactCurrentlyInfected,
-      infectionsByRequestedTime: severeImpactInfectionsByRequestedTime,
-      severeCasesByRequestedTime: severeImpactSevereCasesByRequestedTime,
-      hospitalBedsByRequestedTime: severeImpactHospitalBedsByRequestedTime
-    }
+    impact: {},
+    severeImpact: {}
   };
 };
 
